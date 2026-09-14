019f21d77a67b80818e1a07a2b96ad0b24ae786c
Apo <74223319+Apoteotico@users.noreply.github.com>
2026-09-14 15:04:59 -0300

    Add inlineStylesheets option to build configuration
    se agrega 
    build: {
        inlineStylesheets: 'always'
      },
    
    para incrustar css directo en el html y que astro no tenga que genera un archivo externo, y el navegador tiene que descargarlo antes de pintar la página.
    Esto debería hacer que la web más rápida

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,9 @@ export default defineConfig({
       }
     }
   },
+  build: {
+    inlineStylesheets: 'always'
+  },
   site: "https://lavozcreativaescuela.com.ar",  // URL completa
   base: "/"  // Subcarpeta en GitHub Pages
 });
